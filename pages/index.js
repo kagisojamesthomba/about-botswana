6f560bdbd90d7c1067b032136a4a35929c4147ab
kagiso <kagisojamesthomba@gmail.com>
2021-05-16 20:48:01 +0200

    corrected coming soon spelling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className=' flex m-60 text-7xl content-center justify-center'>
-      CONMING SOON!!!!
+      COMING SOON!!!!
     </div>
   );
 }
